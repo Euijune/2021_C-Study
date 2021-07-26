42bddefbd590120156f49eea9fc442f313b74560
EuiJune <choah76@khu.ac.kr>
2021-07-26 23:33:12 +0900

    Q_3460.cpp

---
+++ C++_STUDY/BAEKJOON/Q_3460.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int* deciTobin(int n) {
+	int num[30] = { 1,2,3, };
+	return num;
+}
+
+void printArray(int* arr) {
+	for (int i = 0; i < 30; i++) {
+		printf("%d ", arr[i]);
+	}
+}
+
+int main() {
+	int N;
+	int* Num;
+	scanf_s("%d", &N);
+
+	Num = deciTobin(N);
+	printArray(Num);
+
+
+	return 0;
+}
