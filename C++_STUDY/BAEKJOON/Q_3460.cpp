c805dba94a6c8d3a5e41918324c5ba10daa40bf9
EuiJune <choah76@khu.ac.kr>
2021-09-01 23:57:46 +0900

    Q 3460

--- C++_STUDY/BAEKJOON/Q_3460.cpp
+++ C++_STUDY/BAEKJOON/Q_3460.cpp
@@ -1,26 +1,44 @@
-#include <stdio.h>
+#include <iostream>
+#include <vector>
+using namespace std;
 
-int* deciTobin(int n) {
-	int num[30] = { 1,2,3, };
-	return num;
-}
-
-void printArray(int* arr) {
-	for (int i = 0; i < 30; i++) {
-		printf("%d ", arr[i]);
+void deci2bin(int N, vector<int>& binary) {
+	if (N == 0) {
+		binary.push_back(0);
+	}
+	else if (N == 1) {
+		binary.push_back(1);
+	}
+	else if (N % 2 == 0) {
+		binary.push_back(0);
+		deci2bin(N / 2, binary);
+	}
+	else if (N % 2 == 1) {
+		binary.push_back(1);
+		deci2bin((N - 1) / 2, binary);
 	}
 }
 
 int main() {
-	int N;
-	int* Num;
-	scanf_s("%d", &N);
+	int T, N;
+	cin >> T;
+	
 
-	Num = deciTobin(N);
-	printArray(Num);
+	while (T--) {
 
-	modify;
+		cin >> N;
 
+		vector<int> binary;
+		deci2bin(N, binary);
 
+		indx = 0
+		for (int elem : binary) {
+			if (elem) {
+				cout << indx << " ";
+			}
+			indx++;
+		}
+	}
+	
 	return 0;
 }
