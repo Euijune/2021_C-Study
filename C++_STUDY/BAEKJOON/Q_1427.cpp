96c33d4a71143b55176a38d61fb8de17a8c4f18f
EuiJune <choah76@khu.ac.kr>
2021-09-14 00:03:01 +0900

    Q 1427

--- C++_STUDY/BAEKJOON/Q_1427.cpp
+++ C++_STUDY/BAEKJOON/Q_1427.cpp
@@ -3,24 +3,22 @@
 using namespace std;
 
 int main() {
-	char N[10];
-	priority_queue <int, vector<int>, greater<int>> Q;
+	char N[11];
+	priority_queue <int, vector<int>, less<int>> Q;
 	cin >> N;
 	
 	for (int i = 0; i < 10; i++) {
 		int n = N[i] - '0';
-		cout << "test : " << n << endl;
 		if (0 <= n && n <= 9) {
 			Q.push(n);
 		}
 	}
 
-	cout << Q.size() << endl;
-	for (int i = 0; i < Q.size(); i++) {
+	int length = Q.size();
+	for (int i = 0; i < length; i++) {
 		cout << Q.top();
 		Q.pop();
 	}
-
 
 	return 0;
 }
