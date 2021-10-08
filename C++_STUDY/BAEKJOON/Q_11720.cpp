94b447eb543ba47fc1e8a0f1ee3f92d0f0ff09f2
EuiJune <choah76@khu.ac.kr>
2021-10-08 23:59:59 +0900

    Q 11720

--- C++_STUDY/BAEKJOON/Q_11720.cpp
+++ C++_STUDY/BAEKJOON/Q_11720.cpp
@@ -2,12 +2,21 @@
 using namespace std;
 
 int main() {
-	char numlist[101];
+	int N;
+	cin >> N;
+
+	char* numlist = new char[N + 1];
+	int sum = 0;
 	cin >> numlist;
 
-	for (char elem : numlist) {
-		cout << elem << " ";
+	for (int i = 0; i < N + 1; i++) {
+		if (numlist[i] == '\0')
+			break;
+		int n = numlist[i] - '0';
+		sum += n;
 	}
-	
+
+	cout << sum;
+
 	return 0;
 }
