7dc7815cf28f5d589625f4a100d741e966c469e7
EuiJune <choah76@khu.ac.kr>
2021-10-16 22:37:46 +0900

    test

--- C++_STUDY/Week5/test1.cpp
+++ C++_STUDY/Week5/test1.cpp
@@ -3,25 +3,7 @@
 
 #incl
 
-#include <stdio.h>
-#incl
-
-#incl
-
-#include <stdio.h>
-#incl
-
-#incl
-sncl
-
-#include <stdio.h>
-#incl
-
-#incl
-s
-ncl
-
-#include <stdio.h>
+#include <s
 #incl
 
 #incl
