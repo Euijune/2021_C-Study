92300ab13b43206310ab4025435c7b2ab280d5fb
EuiJune <choah76@khu.ac.kr>
2021-11-08 23:18:18 +0900

    Quiz 7

--- C++_STUDY/Week5/test1.cpp
+++ C++_STUDY/Week5/test1.cpp
@@ -1,5 +1,19 @@
+#include <iostream>
+using namespace std;
 
-#include <stdio.h>
+int sum2(int upto)
+{
+	if (upto == 1) {
+		return 1;
+	}
+	else {
+		return upto + sum2(upto - 1);
+	}
+}
 
+int main() {
+	cout << sum2(100);
 
+	return 0;
+}
 
