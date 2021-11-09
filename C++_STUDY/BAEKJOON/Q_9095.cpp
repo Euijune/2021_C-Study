254ceeb3a9ff74102d3fb67db7c919a8f476e599
EuiJune <choah76@khu.ac.kr>
2021-11-09 17:49:59 +0900

    Q 9095

---
+++ C++_STUDY/BAEKJOON/Q_9095.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int oneTwoThree(int n);
+
+int main() {
+	int T;
+	cin >> T;
+
+	int input;
+	while (T--) {
+		cin >> input;
+		cout << oneTwoThree(input) << endl;
+	}
+
+	return 0;
+}
+
+int oneTwoThree(int n) {
+
+}
