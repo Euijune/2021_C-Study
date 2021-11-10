8d9b29a484287fe2f6c7bd7d2d0e2038a4028072
EuiJune <choah76@khu.ac.kr>
2021-11-10 22:13:46 +0900

    teststst

--- C++_STUDY/Week5/test1.cpp
+++ C++_STUDY/Week5/test1.cpp
@@ -1,19 +1,3 @@
 #include <iostream>
 using namespace std;
 
-int sum2(int upto)
-{
-	if (upto == 1) {
-		return 1;
-	}
-	else {
-		return upto + sum2(upto - 1);
-	}
-}
-
-int main() {
-	cout << sum2(100);
-
-	return 0;
-}
-
