46b722d4e7613ef900357dbb28cb1f93021ef2ee
EuiJune <choah76@khu.ac.kr>
2021-11-19 01:42:37 +0900

    Q 10816 with lower/upper bound

--- C++_STUDY/BAEKJOON/Q_10816.cpp
+++ C++_STUDY/BAEKJOON/Q_10816.cpp
@@ -3,17 +3,23 @@
 #include <algorithm>
 using namespace std;
 
+int N, M;
+vector<int> vec;
 
-bool comp (const int& rhs, const int& lhs) {
+bool comp(const int& rhs, const int& lhs) {
 	return lhs > rhs;	// 오름차순
 }
 
-int search(int item, vector<int> v);
-
+int search_lowerIdx(int item, vector<int>& v);
+int search_upperIdx(int item, vector<int>& v);
 
 int main() {
-	int N, M;
-	vector<int> vec;
+	// For Fast I/O
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	
 
 	cin >> N;
 	int temp;
@@ -23,20 +29,41 @@ int main() {
 	}
 
 	sort(vec.begin(), vec.end(), comp);
-	cout << vec.size();
 
 	cin >> M;
+	int lower_idx, upper_idx;
 	while (M--) {
 		cin >> temp;
+		lower_idx = search_lowerIdx(temp, vec);
+		upper_idx = search_upperIdx(temp, vec);
+		cout << upper_idx - lower_idx << " ";
 	}
 
 	return 0;
 }
 
-int search(int item, vector<int> v) {
-	int index = 0;
+int search_lowerIdx(int item, vector<int>& v) {
 	int top = 0;
 	int bot = v.size();
 
-	return index;
+	while (top != bot) {
+		int mid = (top + bot) / 2;
+		if (v[mid] >= item)	bot = mid;
+		else				top = mid + 1;
+	}
+	
+	return top;
+}
+
+int search_upperIdx(int item, vector<int>& v) {
+	int top = 0;
+	int bot = v.size();
+
+	while (top != bot) {
+		int mid = (top + bot) / 2;
+		if (v[mid] > item)	bot = mid;
+		else				top = mid + 1;
+	}
+
+	return top;
 }
