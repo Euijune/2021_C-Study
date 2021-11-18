004ac6305474182373549edaa385d68824dbea90
EuiJune <choah76@khu.ac.kr>
2021-11-18 23:54:48 +0900

    Q 10867

--- C++_STUDY/BAEKJOON/Q_10867.cpp
+++ C++_STUDY/BAEKJOON/Q_10867.cpp
@@ -6,6 +6,11 @@ int num_arr[MAX];
 int N;
 
 int main() {
+	// For fast I/O
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL); 
+	std::cout.tie(NULL);
+
 	memset(num_arr, 0, sizeof(int) * MAX);
 	
 	int temp;
