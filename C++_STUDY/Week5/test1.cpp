7c268f6d3c1ad3786d694fa699c31e64c4e6f602
EuiJune <choah76@khu.ac.kr>
2021-12-25 22:34:31 +0900

    testes

--- C++_STUDY/Week5/test1.cpp
+++ C++_STUDY/Week5/test1.cpp
@@ -6,17 +6,7 @@
 
 #include <iostream>
 #include <iostream>
-#include <iostream
-#include <iostream>
-#include <iostream>
-#include <iostream
-#include <iostream>
-#include <iostream>
-#include <iostream
-#include <iostream>
-#include <iostream>
-#include <iostream
-#include <iostream>
+#i
 #include <iostream>
 #include <iostream
 #include <iostream>
