a4eace5722fddadc46ea7d3b78c18fb5e10841a6
EuiJune <choah76@khu.ac.kr>
2022-05-26 15:28:13 +0900

    sorry

--- C++_STUDY/Week5/test1.cpp
+++ C++_STUDY/Week5/test1.cpp
@@ -4,4 +4,20 @@ using namespace std;
 #define R first
 #define C second
 typedef pair<int, int> pii;
+#include <iostream>
+using namespace std;
+
+#define R first
+#define C second
+typedef pair<int, int> pii; #include <iostream>
+using namespace std;
+
+#define R first
+#define C second
+typedef pair<int, int> pii; #include <iostream>
+using namespace std;
+
+#define R first
+#define C second
+typedef pair<int, int> pii;
 
