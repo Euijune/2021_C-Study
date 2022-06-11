4971af13691c82d4489127f488a15d11d8878b6a
EuiJune <choah76@khu.ac.kr>
2022-06-11 23:24:04 +0900

    temp

--- C++_STUDY/Week5/test1.cpp
+++ C++_STUDY/Week5/test1.cpp
@@ -1,21 +1,5 @@
 #include <iostream>
 using namespace std;
 
-#define R first
-#define C second
 
-#include <iostream>
-
-#define R first
-#define C second
-#include <iostream>
-using namespace std;
-
-#define R first
-#define C second
-#include <iostream>
-using namespace std;
-
-#define R first
-#define C second
 
