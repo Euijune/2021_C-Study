aabf0b1f0990abfafd95ff3fe0f88b466597b73b
EuiJune <choah76@khu.ac.kr>
2022-08-02 11:33:51 +0900

    Q 1956

--- C++_STUDY/BAEKJOON/Q_1956.cpp
+++ C++_STUDY/BAEKJOON/Q_1956.cpp
@@ -1,32 +1,12 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 #define MAXV 400
-#define NULL 0
 #define MAXDIST 999999999
 
 int V, E;
-int ans = MAXDIST;
-int graph[MAXV+1][MAXV+1];
-bool visit[MAXV + 1];
-
-void DFS(int start, int curr, int dist) {
-	//cout << "curr: " << curr << endl;
-	visit[curr] = true;
-
-	for (int next = 1; next <= V; next++) {
-		int cost = graph[curr][next];
-		if (cost != NULL) {
-			//cout << "next: " << next << endl;
-			if (next == start)
-				ans = min(ans, dist + cost);
-			if (!visit[next])
-				DFS(start, next, dist + cost);
-		}
-	}
-
-	visit[curr] = false;
-}
+int cost[MAXV][MAXV];
 
 int main() {
 	ios_base::sync_with_stdio(0);
@@ -34,26 +14,32 @@ int main() {
 
 	cin >> V >> E;
 
+	for (int i = 0; i < V; i++)
+		for (int j = 0; j < V; j++)
+			cost[i][j] = MAXDIST;
+
 	for (int i = 0; i < E; i++) {
 		int a, b, c;
 		cin >> a >> b >> c;
-		graph[a][b] = c;
+		cost[--a][--b] = c;
 	}
 
-	/*cout << endl;
-	for (int i = 1; i <= V; i++) {
-		for (int j = 1; j <= V; j++) {
-			cout << graph[i][j] << " ";
+	for (int node = 0; node < V; node++) {
+		for (int s = 0; s < V; s++) {
+			if (s == node)
+				continue;
+			for (int e = 0; e < V; e++) {
+				if (e == node || s == e)
+					continue;
+				cost[s][e] = min(cost[s][e], cost[s][node] + cost[node][e]);
+			}
 		}
-		cout << endl;
 	}
-	cout << endl;*/
 
-	for (int i = 1; i <= V; i++) {
-		DFS(i, i, 0);
-		visit[i] = true;
-		//cout << "ans : " << ans << endl << endl;;
-	}
+	int ans = MAXDIST;
+	for (int i = 0; i < V; i++)
+		for (int j = 0; j < V; j++)
+			ans = min(ans, cost[i][j] + cost[j][i]);
 
 	if (ans == MAXDIST)
 		ans = -1;
